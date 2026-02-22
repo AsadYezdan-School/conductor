2a17d7a2a856c3a3f4ae9ed17c245a286440b014
Asad Yezdan <yezdanasad@gmail.com>
2026-02-22 11:10:08 +0000

    the java is building the Go is not, I need a nix-shell for this repo

--- submitter/src/main/java/com/github/asadyezdanschool/conductor/submitter/Main.java
+++ submitter/src/main/java/com/github/asadyezdanschool/conductor/submitter/Main.java
@@ -3,7 +3,7 @@ package com.github.asadyezdanschool.conductor.submitter;
 
 public class Main{
     static void main() {
-        System.out.println("Hello World");
+        System.out.println("Hello World from Java 25 built with Bazel");
     }
 
 }
