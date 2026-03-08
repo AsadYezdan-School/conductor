4f6b9f046f2cc0ae601319480d8de80966fa71d3
Asad <yezdanasad@gmail.com>
2026-03-08 18:05:35 +0000

    all targets building and containers for everything are being loaded

---
+++ scheduler/src/main/java/com/github/asadyezdanschool/conductor/scheduler/Main.java
@@ -0,0 +1,12 @@
+package com.github.asadyezdanschool.conductor.scheduler;
+
+
+public class Main{
+    static void main() throws InterruptedException {
+        while (true) {
+            System.out.println("Hello World from Java 25 in the scheudler built with Bazel ");
+            Thread.sleep(1000);
+        }
+    }
+
+}
