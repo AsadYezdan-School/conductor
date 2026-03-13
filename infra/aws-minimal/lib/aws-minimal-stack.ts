1ae89392ad0c06ee61d4ec562efc9ed506dbcd4e
Asad <yezdanasad@gmail.com>
2026-03-13 20:36:19 +0000

    rds version to 17.6

--- infra/aws-minimal/lib/aws-minimal-stack.ts
+++ infra/aws-minimal/lib/aws-minimal-stack.ts
@@ -35,7 +35,7 @@ export class AwsMinimalStack extends cdk.Stack {
     const database = new rds.DatabaseInstance(this, 'ConductorDatabase', {
       instanceIdentifier: 'conductor-database',
       engine: rds.DatabaseInstanceEngine.postgres({
-        version: rds.PostgresEngineVersion.VER_16_3,
+        version: rds.PostgresEngineVersion.VER_17_6,
       }),
       instanceType: ec2.InstanceType.of(ec2.InstanceClass.T4G, ec2.InstanceSize.MICRO),
       vpc,
