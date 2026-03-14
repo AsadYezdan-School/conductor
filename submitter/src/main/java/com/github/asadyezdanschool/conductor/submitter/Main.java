46aabd0e763d5ea73d9265a9e3f8bb80e012d368
Asad <yezdanasad@gmail.com>
2026-03-14 17:10:54 +0000

    deploy with shortsha

--- submitter/src/main/java/com/github/asadyezdanschool/conductor/submitter/Main.java
+++ submitter/src/main/java/com/github/asadyezdanschool/conductor/submitter/Main.java
@@ -2,7 +2,7 @@ package com.github.asadyezdanschool.conductor.submitter;
 
 public class Main {
     static String greeting() {
-        return "Hello World from Java 25 built with Bazel after rebuild and moving bazelrc";
+        return "Hello World from Java 25 built with Bazel after rebuild and moving bazelrc, we are trying to deploy with AWS";
     }
 
     public static void main(String[] args) throws InterruptedException {
--- tools/ci/publish_images.sh
+++ tools/ci/publish_images.sh
@@ -10,8 +10,7 @@ if [[ "${BRANCH_NAME}" == release/* ]]; then
 elif [[ "${BRANCH_NAME}" == main ]]; then
     TAG="main-${SHORT_SHA}"
 elif [[ "${BRANCH_NAME}" == dev-* ]]; then
-#    TAG="${BRANCH_NAME}-${SHORT_SHA}"
-     TAG="master"
+    TAG="${BRANCH_NAME}-${SHORT_SHA}"
 else
     echo "No publish rule for branch '${BRANCH_NAME}', skipping."
     echo "Branch name must start with 'dev-'"
