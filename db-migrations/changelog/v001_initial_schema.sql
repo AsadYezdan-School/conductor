7aa765fbc84bcdaec6e2f04e5e4a0a03f8c5f5cc
AsadYezdan-School <yezdanasad@gmail.com>
2026-03-20 20:28:38 +0000

    simplify to roll forward only (#17)
    * simplify to roll forward only
    
    * manual pick migration

--- db-migrations/changelog/v001_initial_schema.sql
+++ db-migrations/changelog/v001_initial_schema.sql
@@ -1,16 +1,15 @@
 --liquibase formatted sql
 
 --changeset conductor:v001, create definition for http jobs
-CREATE TYPE IF NOT EXISTS request_type AS ENUM ('GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS','HEAD');
-CREATE TABLE IF NOT EXISTS http_jobs (
+CREATE TYPE request_type AS ENUM ('GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS','HEAD');
+CREATE TABLE http_jobs (
     id          UUID         PRIMARY KEY DEFAULT gen_random_uuid(),
     name        TEXT         NOT NULL,
     payload     JSONB,
     status      TEXT         NOT NULL DEFAULT 'CREATED',
     created_at  TIMESTAMPTZ  NOT NULL DEFAULT NOW(),
     updated_at  TIMESTAMPTZ  NOT NULL DEFAULT NOW(),
-    cron        TEXT         NOT NULL
-    url         TEXT         NOT NULL
+    cron        TEXT         NOT NULL,
+    url         TEXT         NOT NULL,
     method      request_type NOT NULL
-
 );
